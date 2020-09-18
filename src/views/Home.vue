bf29da9c72c8c998f3bcd0859f429bd4547ef3b1
Abel Masila <abelmasila@gmail.com>
2020-09-18 18:03:58 +0300

    show homepage buttons if not logged in

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,13 +24,19 @@
             <a href="https://firebase.google.com">Firebase</a>.
           </p>
 
-          <router-link class="btn btn-outline-primary mr-2" to="/register"
+          <router-link
+            class="btn btn-outline-primary mr-2"
+            to="/register"
+            v-if="!user"
             >Register</router-link
           >
-          <router-link class="btn btn-outline-primary mr-2" to="/login"
+          <router-link
+            class="btn btn-outline-primary mr-2"
+            to="/login"
+            v-if="!user"
             >Log In</router-link
           >
-          <router-link class="btn btn-primary" to="/meetings"
+          <router-link class="btn btn-primary" to="/meetings" v-if="user"
             >Meetings</router-link
           >
         </div>
